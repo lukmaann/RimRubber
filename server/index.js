6f87e85c7e47ce16a93f8cdcdb8c89848010f731
LNUAKX <lukmaannadaf@gmail.com>
2023-10-24 12:13:09 +0530

    some changes in indexjs

--- server/index.js
+++ server/index.js
@@ -24,6 +24,14 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://rimrubber.netlify.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ extended: true }));
 app.use(express.json());

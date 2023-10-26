81fe34a0dbd89b1c6a7461935af6d70a9f61b533
LNUAKX <lukmaannadaf@gmail.com>
2023-10-26 15:58:33 +0530

    modify

--- client/src/pages/myads/myads.jsx
+++ client/src/pages/myads/myads.jsx
@@ -2,14 +2,15 @@ import Header from "../../components/Header/header";
 import Footer from "../../components/Footer/footer";
 import Style from "./myads.module.css";
 import { Toaster } from "react-hot-toast";
-// import { getAds } from "../../helper/helper";
 import useFecthMyAds from "../../hooks/usefecthad";
 import { sampledata } from "../../../data/sample";
 import { myAdsStore } from "../../store/store";
 import AdComponent from "../../components/ads/ad-component";
+import { useNavigate } from "react-router-dom";
 
 const Myads = () => {
   const [getdata] = useFecthMyAds();
+  const navigate=useNavigate()
   const myads=myAdsStore((state)=>state.ads);
   // const myads = sampledata;
 
@@ -21,7 +22,7 @@ const Myads = () => {
         <Header />
 
         <div className={Style.topdiv}>
-          <h1>Loading</h1>
+          <h1 className=" text-center">Loading...</h1>
         </div>
         <Footer />
       </div>
@@ -33,7 +34,17 @@ const Myads = () => {
         <Header />
 
         <div className={Style.topdiv}>
-          {myads.map((item, index) => {
+          {myads.length===0?<div className="flex w-[100%] h-[100%] text-xl capitalize justify-center items-center">
+
+
+          <div>
+            
+         <h1> You havent posted Anything yet</h1>
+         <button onClick={()=>navigate('/sellitem')} className="bg-yellow-400 rounded-sm p-2  text-black my-5 ">Sell your Tyre</button>
+          </div>
+          
+          
+          </div>:myads.map((item, index) => {
             return (
               <AdComponent
                 key={index}

844c36821033bf5687d76bc704695eb823bffe3a
lukmaan <lukmaannadaf@gmail.com>
2023-10-30 22:10:41 +0530

    Update header.jsx

--- client/src/components/Header/header.jsx
+++ client/src/components/Header/header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
       <div>
         <ul className="flex">
           {authenticated && (
-            <li className={Style.navItem}>
+            <li className={`${Style.navItem}  text-red-500`}>
               <button
                 onClick={() => {
                   navigate("/feedback");

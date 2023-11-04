84e7005330a96bac31a2a079361ecf4918cfd86e
lukmaan <lukmaannadaf@gmail.com>
2023-11-03 22:16:24 -0700

    added spinner

--- client/src/adminModule/dashboard/dashboard.jsx
+++ client/src/adminModule/dashboard/dashboard.jsx
@@ -5,15 +5,29 @@ import AdminHeader from "../adminComponents/admin-header/adminheader";
 import ReviewAds from "../adminComponents/reviewads/reviewad";
 import { useFecthAds } from "../../hooks/usefecthad";
 import { myAdsStore } from "../../store/store";
+import { ColorRing } from "react-loader-spinner";
 const AdminDashboard = () => {
 
   
   const {ads}=myAdsStore((state)=>state)
-  const [{apiData,isLoading}]=useFecthAds({type:'pending'});
+  const [{isLoading}]=useFecthAds({type:'pending'});
 
   
 if(isLoading){
-  return <div>Loading</div>
+  return <div className={Style.main}>
+      <div className="w-[100%] h-[200px] justify-center items-center flex">
+          <ColorRing
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+            colors={['#111827','#111827','#111827','#111827','#111827']}
+          />
+          </div>
+    
+  </div>
 }else{
 
 
--- client/src/pages/myads/myads.jsx
+++ client/src/pages/myads/myads.jsx
@@ -2,16 +2,17 @@ import Header from "../../components/Header/header";
 import Footer from "../../components/Footer/footer";
 import Style from "./myads.module.css";
 import { Toaster } from "react-hot-toast";
-import {useFecthMyAds} from "../../hooks/usefecthad";
+import { useFecthMyAds } from "../../hooks/usefecthad";
 // import { sampledata } from "../../data/sample";
 import { myAdsStore } from "../../store/store";
 import AdComponent from "../../components/ads/ad-component";
 import { useNavigate } from "react-router-dom";
+import { ColorRing } from 'react-loader-spinner'
 
 const Myads = () => {
   const [getdata] = useFecthMyAds();
-  const navigate=useNavigate()
-  const myads=myAdsStore((state)=>state.ads);
+  const navigate = useNavigate()
+  const myads = myAdsStore((state) => state.ads);
   // const myads = sampledata;
 
 
@@ -22,7 +23,17 @@ const Myads = () => {
         <Header />
 
         <div className={Style.topdiv}>
-          <h1 className=" text-center">Loading...</h1>
+          <div className="w-[100%] justify-center items-center flex">
+          <ColorRing
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+            colors={['#111827','#111827','#111827','#111827','#111827']}
+          />
+          </div>
         </div>
         <Footer />
       </div>
@@ -34,17 +45,17 @@ const Myads = () => {
         <Header />
 
         <div className={Style.topdiv}>
-          {myads.length===0?<div className="flex w-[100%] h-[100%] text-xl capitalize justify-center items-center">
+          {myads.length === 0 ? <div className="flex w-[100%] h-[100%] text-xl capitalize justify-center items-center">
 
 
-          <div>
-            
-         <h1> You havent posted Anything yet</h1>
-         <button onClick={()=>navigate('/sellitem')} className=" border  rounded-sm p-2 capitalize text-white my-5 ">Start selling now</button>
-          </div>
-          
-          
-          </div>:myads.map((item, index) => {
+            <div>
+
+              <h1> You havent posted Anything yet</h1>
+              <button onClick={() => navigate('/sellitem')} className=" border  rounded-sm p-2 capitalize text-white my-5 ">Start selling now</button>
+            </div>
+
+
+          </div> : myads.map((item, index) => {
             return (
               <AdComponent
                 key={index}

4f0636ca8c33f86f405495b1bc5275a640d9b690
LNUAKX <lukmaannadaf@gmail.com>
2023-11-13 17:18:23 +0530

    added loading page

--- client/src/pages/bootingup/bootingUp.jsx
+++ client/src/pages/bootingup/bootingUp.jsx
@@ -3,6 +3,7 @@ import Tyre from "../../assets/tyre.png";
 import { useEffect, useState } from "react";
 import { loadServer } from "../../helper/loginHelper";
 import { useNavigate } from "react-router-dom";
+import toast, {Toaster} from "react-hot-toast"
 const BootingUp = () => {
   const [text, setText] = useState("booting");
   const navigate = useNavigate();
@@ -17,16 +18,37 @@ const BootingUp = () => {
       }, 2000);
     });
     setTimeout(() => {
-      setText("Setting Up");
+      setText("Setting Up The Servers");
     }, 4000);
 
     setTimeout(() => {
-      setText("Server taking to long to respond please refresh the page");
-    }, 10000);
+      setText("Slow network detected");
+    }, 9000);
+
+    setTimeout(() => {
+      setText("Server taking to long");
+      toast((t) => (
+        <span className="w-full " >
+          
+          <button  className="w-full" onClick={() => {
+            location.reload()
+            toast.dismiss(t.id)}}>
+           Reload
+          </button>
+        </span>
+      ),{
+        style:{
+          width:'90vw',
+          border:'1px solid #713200',
+          color: '#713200'
+        }
+      });
+    }, 13000);
   }, []);
 
   return (
     <div className={Style.main}>
+    <Toaster position="bottom-right"/>
       <img src={Tyre} alt="" className={Style.tyre} />
       <h1 className={Style.txt}>{text}</h1>
     </div>
